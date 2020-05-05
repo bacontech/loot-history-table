6058ca17f836c205a977709f25a22dbfd23a06b8
Alex Bacon <fosizzle@gmail.com>
2020-05-05 08:22:09 -0500

    Add checkbox for bis and need only

--- src/components/LootPage.vue
+++ src/components/LootPage.vue
@@ -2,6 +2,17 @@
   <div class="loot-page">
     <h1>Tegrity Loot History</h1>
     <br>
+    <div>
+
+    </div>
+    <div class="checkbox-container">
+      <label class="checkbox-component">
+        Bis & Need Only
+        <input id="bis" type="checkbox" value="bisNeed" v-model="checkedOptions">
+        <span class="checkmark"></span>
+      </label>
+
+    </div>
     <vue-good-table
       :columns="columns"
       :rows="rows"
@@ -34,6 +45,7 @@
     },
     data () {
       return {
+        checkedOptions: ['bisNeed'],
         csvData: null
       };
     },
@@ -89,8 +101,12 @@
         ]
       },
       rows   () {
-        console.log(this.$store.getters.getPlayerLootForSearchTable)
-        return this.$store.getters.getPlayerLootForSearchTable
+        if (this.checkedOptions.includes('bisNeed')) {
+          return this.$store.getters.getPlayerLootForSearchTableBisNeedOnly
+        } else {
+          return this.$store.getters.getPlayerLootForSearchTable
+        }
+
       }
     },
     methods: {
@@ -122,4 +138,78 @@
   a {
     color: #42b983;
   }
+
+  /* Start CHECKBOX CSS */
+  /* Hide the browser's default checkbox */
+  .checkbox-container {
+    width: 30%;
+    margin: auto;
+  }
+
+  .checkbox-component {
+    display: block;
+    position: relative;
+    padding-left: 35px;
+    margin-bottom: 12px;
+    cursor: pointer;
+    font-size: 22px;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+  }
+
+  .checkbox-component input {
+    position: absolute;
+    opacity: 0;
+    cursor: pointer;
+    height: 0;
+    width: 0;
+  }
+
+  /* Create a custom checkbox */
+  .checkmark {
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 25px;
+    width: 25px;
+    background-color: #eee;
+  }
+
+  /* On mouse-over, add a grey background color */
+  .checkbox-component:hover input ~ .checkmark {
+    background-color: #ccc;
+  }
+
+  /* When the checkbox is checked, add a blue background */
+  .checkbox-component input:checked ~ .checkmark {
+    background-color: #2196F3;
+  }
+
+  /* Create the checkmark/indicator (hidden when not checked) */
+  .checkmark:after {
+    content: "";
+    position: absolute;
+    display: none;
+  }
+
+  /* Show the checkmark when checked */
+  .checkbox-component input:checked ~ .checkmark:after {
+    display: block;
+  }
+
+  /* Style the checkmark/indicator */
+  .checkbox-component .checkmark:after {
+    left: 9px;
+    top: 5px;
+    width: 5px;
+    height: 10px;
+    border: solid white;
+    border-width: 0 3px 3px 0;
+    -webkit-transform: rotate(45deg);
+    -ms-transform: rotate(45deg);
+    transform: rotate(45deg);
+  }
+  /* End CHECKBOX CSS */
 </style>
--- src/store/getters.js
+++ src/store/getters.js
@@ -17,6 +17,28 @@ const getPlayerLootForSearchTable = state => {
   return dataForTable
 }
 
+const getPlayerLootForSearchTableBisNeedOnly = state => {
+  if (state.players === {}) {
+    return []
+  }
+  let dataForTable = []
+
+  Object.keys(state.players).forEach(playerName => {
+    const player = state.players[playerName]
+
+    const playerLoot = player.loot.filter(loot => loot.rollType === 'Mainspec /Need' || loot.rollType === 'Best Available')
+
+    dataForTable.push({
+      mode: 'span',
+      label: playerName,
+      html: false,
+      children: playerLoot
+    })
+  })
+  return dataForTable
+}
+
 export default {
-  getPlayerLootForSearchTable
+  getPlayerLootForSearchTable,
+  getPlayerLootForSearchTableBisNeedOnly
 };
